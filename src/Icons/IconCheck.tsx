763c1496fd2fb5b2dc12deda6807017dbd0e72d8
evertzner <esteban.vertzner@gmail.com>
2024-08-20 11:32:38 +0200

    fix props name

--- src/Icons/IconCheck.tsx
+++ src/Icons/IconCheck.tsx
@@ -1,7 +1,7 @@
 const IconCheck = () => {
   return (
     <svg width='15' height='11' viewBox='0 0 15 11' fill='none'>
-      <path d='M1 5.5L4.95263 9.45263L13.4053 1' stroke='#161932' stroke-width='2'></path>
+      <path d='M1 5.5L4.95263 9.45263L13.4053 1' stroke='#161932' strokeWidth='2'></path>
     </svg>
   );
 };
